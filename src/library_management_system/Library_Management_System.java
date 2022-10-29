abfd26d497a9b4f6a2b6200858514b2f8b93c4bc
krishnam1912 <54627911+krishnam1912@users.noreply.github.com>
2022-10-29 16:52:27 +0530

    Update Library_Management_System.java

--- src/library_management_system/Library_Management_System.java
+++ src/library_management_system/Library_Management_System.java
@@ -16,6 +16,7 @@ public class Library_Management_System {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         HomePage n=new HomePage();
     }
     
 }
